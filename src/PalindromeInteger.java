8b22ad941761da4aa59d19ec54aa27e30a1e0d56
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-01 19:56:48 +0700

    run to check if a number is a palindrome integer or not

---
+++ src/PalindromeInteger.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PalindromeInteger {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        System.out.println("Please enter a number");
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+
+        System.out.println(isPalinddromeInteger(a));
+
+
+    }
+    public static int reverse(int a) {
+        int b = 0;
+        int c;
+        for (int i = 0; i < a; i++) {
+            while (a > 0) {
+                c = a % 10;
+                b = b * 10 + c;
+                a /= 10;
+            }
+        }
+        return b;
+    }
+
+    public static boolean isPalinddromeInteger(int a) {
+        if (a == reverse(a)) {
+            System.out.println(a + " is a palindrome number");
+            return true;
+        }
+        else {
+            System.out.println(a + " is not a palindrome number");
+            return false;
+        }
+    }
+}
